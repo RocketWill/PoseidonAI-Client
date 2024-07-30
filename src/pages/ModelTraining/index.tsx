66843ae218af9412130129f2a05fb01714ce2d84
Will Cheng <will.cheng@efctw.com>
2024-07-30 17:10:48 +0800

    feat: Add index page for TrainingModel

--- src/pages/ModelTraining/index.tsx
+++ src/pages/ModelTraining/index.tsx
@@ -1,18 +1,36 @@
 /*
  * @Author: Will Cheng (will.cheng@efctw.com)
  * @Date: 2024-07-29 16:22:28
- * @LastEditors: Will Cheng chengyong@pku.edu.cn
- * @LastEditTime: 2024-07-29 20:30:31
+ * @LastEditors: Will Cheng (will.cheng@efctw.com)
+ * @LastEditTime: 2024-07-30 17:07:18
  * @FilePath: /PoseidonAI-Client/src/pages/ModelTraining/index.tsx
  */
 import { PageContainer } from '@ant-design/pro-components';
 import React from 'react';
-import GpuState from './components/GpuStatus';
+import TrainingConfigForm from './components/TrainingConfigForm';
+
+import { TrainingFrameworkItem } from '../Configuration';
+
+export interface DetectTypeItem {
+  _id: string;
+  name: string;
+  tag_name: string;
+  description: string;
+}
+
+export interface AlgorithmItem {
+  _id: string;
+  name: string;
+  description: string;
+  created_at: string;
+  detect_type: DetectTypeItem;
+  training_framework: TrainingFrameworkItem;
+}
 
 const ModelTraining: React.FC = () => {
   return (
     <PageContainer>
-      <GpuState />
+      <TrainingConfigForm />
     </PageContainer>
   );
 };
