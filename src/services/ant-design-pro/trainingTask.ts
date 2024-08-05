08aa4854433b0c79556af3113d9a82117ee25beb
Will Cheng <will.cheng@efctw.com>
2024-08-05 16:32:38 +0800

    feat: add training tasks apis

---
+++ src/services/ant-design-pro/trainingTask.ts
@@ -0,0 +1,23 @@
+import token from '@/utils/token';
+import { request } from '@umijs/max';
+
+export async function createTask(body: any) {
+  return request('/api/training-tasks/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${token.get()}`,
+    },
+    data: body,
+  });
+}
+
+export async function getCreateTaskStatus(taskId: string) {
+  return request(`/api/training-tasks/task-create-status/${taskId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      // Authorization: `Bearer ${token.get()}`,
+    },
+  });
+}
