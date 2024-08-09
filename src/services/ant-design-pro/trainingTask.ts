d84fefa55f6637b97c1a47ebf9c32f4a66398476
Will Cheng <will.cheng@efctw.com>
2024-08-09 16:09:46 +0800

    feat: Add functions for managing training tasks
    - Added `startTraining` function to initiate training for a specific task.
    - Added `getTrainingStatus` function to retrieve the status of a training task.
    - Added `stopTraining` function to stop an ongoing training task.
    - Updated existing task-related functions to ensure consistent usage of authorization headers.
    - Cleaned up and organized the file for better readability.

--- src/services/ant-design-pro/trainingTask.ts
+++ src/services/ant-design-pro/trainingTask.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Will Cheng (will.cheng@efctw.com)
+ * @Date: 2024-08-05 11:28:13
+ * @LastEditors: Will Cheng (will.cheng@efctw.com)
+ * @LastEditTime: 2024-08-08 16:38:02
+ * @FilePath: /PoseidonAI-Client/src/services/ant-design-pro/trainingTask.ts
+ */
 import token from '@/utils/token';
 import { request } from '@umijs/max';
 
@@ -35,6 +42,48 @@ export async function listUserTasks() {
 export async function getUserTask(taskId: string) {
   return request(`/api/training-tasks/list/${taskId}`, {
     method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token.get()}`,
+    },
+  });
+}
+
+export async function startTraining(taskId: string) {
+  return request(`/api/training-tasks/train/${taskId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token.get()}`,
+    },
+  });
+}
+
+export async function getTrainingStatus(
+  trainingTaskId: string,
+  algoName: string,
+  frameworkName: string,
+  saveKey: string,
+) {
+  return request(
+    `/api/training-tasks/train-status/${trainingTaskId}/${algoName}/${frameworkName}/${saveKey}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token.get()}`,
+      },
+    },
+  );
+}
+
+export async function stopTraining(
+  trainingTaskId: string,
+  algoName: string,
+  frameworkName: string,
+) {
+  return request(`/api/training-tasks/train/${trainingTaskId}/${algoName}/${frameworkName}`, {
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token.get()}`,
