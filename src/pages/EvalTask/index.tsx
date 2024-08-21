0fadcbcbf95f72a7110fad8c227d585a088f19e9
Will Cheng <will.cheng@efctw.com>
2024-08-21 17:04:51 +0800

    chore: Enhance evaluation task with notification and result display
    - Integrated notification system to handle success and error feedback during evaluation.
    - Added periodic polling for evaluation status to update the UI and display results automatically upon completion.
    - Display evaluation results including PR, F1, Precision, and Recall charts after successful evaluation.
    - Improved user experience by disabling and loading button states during ongoing evaluation actions.

--- src/pages/EvalTask/index.tsx
+++ src/pages/EvalTask/index.tsx
@@ -1,13 +1,59 @@
-import { evaluationTask, getEvaluationStatus } from '@/services/ant-design-pro/trainingTask';
+import {
+  evaluationTask,
+  getEvaluationResults,
+  getEvaluationStatus,
+} from '@/services/ant-design-pro/trainingTask';
+import { Col, Collapse, notification, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { TaskItem } from '../TrainingTask';
 import TaskDetailsDescription from '../TrainingTask/components/TaskDetailsDescription';
 import ActionButtons from './components/ActionButtons';
+import F1ConfidenceChart from './components/Charts/F1ConfidenceChart';
+import PRChart from './components/Charts/PRCurves';
+import PrecisionConfidenceChart from './components/Charts/PrecisionConfidenceChart ';
+import ReasultDict from './components/Charts/ReasultDict';
+import RecallConfidenceChart from './components/Charts/RecallConfidenceChart';
 import ModelInferenceForm from './components/ModelInferenceForm';
 
 // 定義 EvalTaskProps 介面，指定 taskData 的類型
 interface EvalTaskProps {
   taskData: TaskItem;
+}
+
+export interface NameItem {
+  id: number;
+  name: string;
+}
+
+export interface MetricsItem {
+  names: NameItem[];
+  confidence: number[];
+  f1: {
+    f1_scores: number[][];
+    f1_mean: number[];
+  };
+  precision: {
+    precision: number[][];
+    precision_mean: number[];
+  };
+  recall: {
+    recall: number[][];
+    recall_mean: number[];
+  };
+  pr: {
+    recall: number[];
+    precision: number[][];
+    precision_mean: number[];
+  };
+  result_dict: {
+    precision: number;
+    recall: number;
+  };
+  parameters: {
+    iou_thres: number;
+    gpu_id: number;
+    batch_size: number;
+  };
 }
 
 // 定義表單數值的介面
@@ -27,6 +73,8 @@ export type EvalStatus =
   | 'ERROR'
   | 'REVOKED';
 
+export type EvalAction = 'restart' | 'start' | 'idle';
+
 // 初始化表單數值
 const initialState: FormValues = {
   iou: 0.3,
@@ -34,10 +82,32 @@ const initialState: FormValues = {
   batchSize: 1,
 };
 
+const fetchEvalResult = async (
+  taskId: string,
+  setMetrics: (d: MetricsItem) => void,
+  notification: any,
+) => {
+  try {
+    const resp = await getEvaluationResults(taskId);
+    setMetrics(resp.results);
+  } catch (err) {
+    console.error(err);
+    notification.error({
+      message: '讀取評估文件失敗',
+      description: '請稍後重試',
+      placement: 'topLeft',
+    });
+  }
+};
+
 const EvalTask: React.FC<EvalTaskProps> = ({ taskData }) => {
   // 使用 useState 管理表單數值和評估任務 ID 的狀態
+  const [api, contextHolder] = notification.useNotification();
   const [formValues, setFormValues] = useState<FormValues>(initialState);
+  const [isEvaling, setIsEvaling] = useState<boolean>(false);
   const [evalId, setEvalId] = useState<string | null>(null);
+  const [metrics, setMetrics] = useState<MetricsItem>();
+  const [currentAction, setCurrentAction] = useState<EvalAction>('idle');
 
   // 清理和格式化演算法名稱和框架名稱
   const algoName: string = taskData.task_detail.algorithm.name.replace(/\s+/g, '');
@@ -58,10 +128,17 @@ const EvalTask: React.FC<EvalTaskProps> = ({ taskData }) => {
 
         // 當任務完成或發生錯誤時停止輪詢
         if (state === 'SUCCESS' || state === 'ERROR' || state === 'FAILURE') {
+          setMetrics(resp.results.data.results);
           if (intervalId) {
             clearInterval(intervalId); // 停止輪詢
             intervalId = null;
           }
+          setIsEvaling(false);
+          setCurrentAction('idle');
+          api.success({
+            message: '模型評估完成',
+            placement: 'topLeft',
+          });
         }
       }
     };
@@ -79,22 +156,64 @@ const EvalTask: React.FC<EvalTaskProps> = ({ taskData }) => {
     };
   }, [evalId, algoName, frameworkName]);
 
+  useEffect(() => {
+    fetchEvalResult(taskData.task_detail._id, setMetrics, api);
+  }, []);
+
   // 處理開始評估的按鈕點擊事件
-  const handleStartEval = async (body: FormValues) => {
+  const handleStartEval = async (body: FormValues, action: EvalAction) => {
+    setIsEvaling(true);
     const taskId: string = taskData.task_detail._id;
     const resp = await evaluationTask(taskId, body);
     setEvalId(resp.results); // 保存 evalId 以啟動輪詢
+    setCurrentAction(action);
   };
 
   return (
     <>
-      <ActionButtons status={undefined} formValues={formValues} handleStartEval={handleStartEval} />
-      <TaskDetailsDescription taskData={taskData} status={undefined} />
-      <ModelInferenceForm
-        styles={{ marginTop: 15 }}
+      {contextHolder}
+      <ActionButtons
+        status={metrics ? 'SUCCESS' : 'IDLE'}
         formValues={formValues}
-        setFormValues={setFormValues}
+        handleStartEval={handleStartEval}
+        isEvaling={isEvaling}
+        currentAction={currentAction}
       />
+      {metrics && <ReasultDict metrics={metrics} style={{ marginBottom: 15 }} />}
+      <TaskDetailsDescription taskData={taskData} status={undefined} />
+      <Collapse
+        defaultActiveKey={['1']}
+        items={[
+          {
+            key: '1',
+            label: 'Inference and Evaluation settings',
+            children: <ModelInferenceForm formValues={formValues} setFormValues={setFormValues} />,
+          },
+        ]}
+        style={{ marginTop: 15 }}
+      />
+
+      {metrics && (
+        <>
+          <Row gutter={16} style={{ marginTop: 15, minWidth: 1000 }}>
+            <Col span={12}>
+              <PRChart metrics={metrics} />
+            </Col>
+            <Col span={12}>
+              <F1ConfidenceChart metrics={metrics} />
+            </Col>
+          </Row>
+
+          <Row gutter={16} style={{ marginTop: 15, minWidth: 1000 }}>
+            <Col span={12}>
+              <PrecisionConfidenceChart metrics={metrics} />
+            </Col>
+            <Col span={12}>
+              <RecallConfidenceChart metrics={metrics} />
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   );
 };
