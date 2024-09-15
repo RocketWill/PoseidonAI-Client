23f80aec192c6f453c75174c88c9b55c58e9fa05
Will Cheng <chengyong@pku.edu.cn>
2024-09-15 19:34:40 +0800

    feat: Add basic PythonDemo component
    - Created `PythonDemo` component to display a placeholder text 'python demo' for Python deployment instructions.

---
+++ src/pages/ExportModel/components/tutorials/PythonDemo.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Will Cheng chengyong@pku.edu.cn
+ * @Date: 2024-09-15 14:55:43
+ * @LastEditors: Will Cheng chengyong@pku.edu.cn
+ * @LastEditTime: 2024-09-15 15:48:44
+ * @FilePath: /PoseidonAI-Client/src/pages/ExportModel/components/tutorials/PythonDemo.tsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved.
+ */
+import React from 'react';
+
+const PythonDemo: React.FC = () => {
+  return 'python demo';
+};
+
+export default PythonDemo;
