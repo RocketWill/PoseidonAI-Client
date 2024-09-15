46e8e03ed9f15432042f1106609fc7faffb32c94
Will Cheng <chengyong@pku.edu.cn>
2024-09-15 19:34:59 +0800

    feat: Add basic CppDemo component
    - Created `CppDemo` component to display a placeholder text 'cpp demo' for C++ deployment instructions.

---
+++ src/pages/ExportModel/components/tutorials/CppDemo.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Will Cheng chengyong@pku.edu.cn
+ * @Date: 2024-09-15 14:55:43
+ * @LastEditors: Will Cheng chengyong@pku.edu.cn
+ * @LastEditTime: 2024-09-15 15:48:40
+ * @FilePath: /PoseidonAI-Client/src/pages/ExportModel/components/tutorials/CppDemo.tsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved.
+ */
+import React from 'react';
+
+const CppDemo: React.FC = () => {
+  return 'cpp demo';
+};
+
+export default CppDemo;
