4cd2f9fa19ef811c82a8114bfc3b3833d0de37c9
Will Cheng <will.cheng@efctw.com>
2024-10-01 13:09:52 +0800

    refactor: optimize layout and runtime configurations
    - Streamlined layout configuration by removing unused links and comments.
    - Enhanced the `childrenRender` function with conditional settings for the `SettingDrawer`.
    - Refined `getInitialState` to handle fetch and error scenarios more effectively.
    - Aligned `request` with the updated error handling from `requestErrorConfig`.

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
-import { Footer, Question, SelectLang, AvatarDropdown, AvatarName } from '@/components';
-import { LinkOutlined } from '@ant-design/icons';
+import { AvatarDropdown, AvatarName, Footer, Question, SelectLang } from '@/components';
+import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
 import type { Settings as LayoutSettings } from '@ant-design/pro-components';
 import { SettingDrawer } from '@ant-design/pro-components';
 import type { RunTimeLayoutConfig } from '@umijs/max';
-import { history, Link } from '@umijs/max';
+import { history } from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
 import { errorConfig } from './requestErrorConfig';
-import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
-import React from 'react';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
@@ -91,10 +89,10 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     ],
     links: isDev
       ? [
-          <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
-            <LinkOutlined />
-            <span>OpenAPI 文档</span>
-          </Link>,
+          // <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
+          //   <LinkOutlined />
+          //   <span>OpenAPI 文档</span>
+          // </Link>,
         ]
       : [],
     menuHeaderRender: undefined,
