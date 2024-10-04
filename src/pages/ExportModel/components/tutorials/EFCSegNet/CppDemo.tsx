0e349bca0d08a3667b4e45883090251860ad1144
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:11:31 +0800

    feat: add placeholder for C++ deployment tutorial for EFC DetNet model
    - Implemented a placeholder component for C++ demo of EFC DetNet model integration

---
+++ src/pages/ExportModel/components/tutorials/EFCSegNet/CppDemo.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Will Cheng chengyong@pku.edu.cn
+ * @Date: 2024-09-15 14:55:43
+ * @LastEditors: Will Cheng (will.cheng@efctw.com)
+ * @LastEditTime: 2024-10-04 14:59:42
+ * @FilePath: /PoseidonAI-Client/src/pages/ExportModel/components/tutorials/EFCDetNet/CppDemo.tsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved.
+ */
+import React from 'react';
+
+const CppDemo: React.FC = () => {
+  return 'cpp demo';
+};
+
+export default CppDemo;
