5c43a5c79b33f5b19648c20bf03acd874cf4834a
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:19:01 +0800

    feat: add dynamic tutorials based on detection type in ExportModel
    - Added logic to display the correct tutorial component (DetNetTutorials or SegNetTutorials) based on the detection type.
    - Defaulted to DetNetTutorials if detection type is not specified.

--- src/pages/ExportModel/index.tsx
+++ src/pages/ExportModel/index.tsx
@@ -2,8 +2,8 @@
 /*
  * @Author: Will Cheng chengyong@pku.edu.cn
  * @Date: 2024-09-08 19:54:02
- * @LastEditors: Will Cheng chengyong@pku.edu.cn
- * @LastEditTime: 2024-09-15 15:32:04
+ * @LastEditors: Will Cheng (will.cheng@efctw.com)
+ * @LastEditTime: 2024-10-04 16:10:05
  * @FilePath: /PoseidonAI-Client/src/pages/ExportModel/index.tsx
  * @Description:
  *
@@ -12,17 +12,31 @@
 import React from 'react';
 import { TaskItem } from '../TrainingTask';
 import ExportModelForm from './components/ExportModelForm';
-import Tutorials from './components/tutorials/Tutorials';
+import DetNetTutorials from './components/tutorials/EFCDetNet/DetNetTutorials';
+import SetNetTutorials from './components/tutorials/EFCSegNet/SegNetTutorials';
 
 export interface ExportModelProps {
   taskData: TaskItem | undefined;
 }
 
 const ExportModel: React.FC<ExportModelProps> = ({ taskData }) => {
+  const detectTypeName: string = taskData?.task_detail.algorithm.detect_type.tag_name || '';
+  let tutorialsComponent;
+  switch (detectTypeName) {
+    case 'det':
+      tutorialsComponent = <DetNetTutorials style={{ marginTop: 20, maxWidth: 900 }} />;
+      break;
+    case 'seg':
+      tutorialsComponent = <SetNetTutorials style={{ marginTop: 20, maxWidth: 900 }} />;
+      break;
+    default:
+      tutorialsComponent = <DetNetTutorials style={{ marginTop: 20, maxWidth: 900 }} />;
+  }
+
   return (
     <>
       <ExportModelForm taskData={taskData} style={{ maxWidth: 900 }} />
-      <Tutorials style={{ marginTop: 20, maxWidth: 900 }} />
+      {tutorialsComponent}
     </>
   );
 };
