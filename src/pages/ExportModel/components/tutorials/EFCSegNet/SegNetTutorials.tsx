15e20f4c2162fac285ce1dc7e7b3db61c2776a51
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:10:32 +0800

    feat: add SegNet model deployment tutorial with language selection
    - Implemented SegNetTutorials component for EFC deep learning model deployment
    - Added language selection for Python, C#, and C++ tutorials
    - Included demo content for each selected language

---
+++ src/pages/ExportModel/components/tutorials/EFCSegNet/SegNetTutorials.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable */
+/*
+ * @Author: Will Cheng chengyong@pku.edu.cn
+ * @Date: 2024-09-08 19:54:02
+ * @LastEditors: Will Cheng (will.cheng@efctw.com)
+ * @LastEditTime: 2024-10-04 16:01:55
+ * @FilePath: /PoseidonAI-Client/src/pages/ExportModel/components/tutorials/EFCSegNet/SegNetTutorials.tsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved.
+ */
+import { DeploymentUnitOutlined } from '@ant-design/icons';
+import { Card, Divider, Radio, Typography } from 'antd';
+import React, { CSSProperties, useState } from 'react';
+import CppDemo from './CppDemo';
+import CSharpDemo from './CSharpDemo';
+import PythonDemo from './PythonDemo';
+
+interface TutorialsProps {
+  style: CSSProperties | undefined;
+}
+type SupportedLanguages = 'python' | 'csharp' | 'cpp';
+
+const demoContent = {
+  python: <PythonDemo />,
+  csharp: <CSharpDemo />,
+  cpp: <CppDemo />,
+};
+
+const SetNetTutorials: React.FC<TutorialsProps> = ({ style }) => {
+  const [selectedLanguage, setSelectedLanguage] = useState<SupportedLanguages>('csharp');
+
+  return (
+    <Card style={style}>
+      <Typography.Title level={4}>
+        <DeploymentUnitOutlined style={{ marginRight: '8px' }} />
+        模型部署
+      </Typography.Title>
+      <Typography.Paragraph>了解如何使用不同的程式語言部署 EFC 深度學習模型。</Typography.Paragraph>
+      <Divider />
+
+      <Typography.Title level={5}>選擇部署語言</Typography.Title>
+      <Radio.Group
+        value={selectedLanguage}
+        onChange={(e) => setSelectedLanguage(e.target.value)}
+        buttonStyle="solid"
+      >
+        <Radio.Button value="python">Python</Radio.Button>
+        <Radio.Button value="csharp">C#</Radio.Button>
+        <Radio.Button value="cpp">C++</Radio.Button>
+      </Radio.Group>
+
+      <Divider />
+
+      {demoContent[selectedLanguage]}
+    </Card>
+  );
+};
+
+export default SetNetTutorials;
